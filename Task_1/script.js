48de992a73a9b66937451c2e68e7a54750e5b50c
saurov-paul <saurov.paul1@gmail.com>
2024-01-19 13:58:47 +0200

    delete

--- Task_1/script.js
+++ Task_1/script.js
@@ -25,3 +25,21 @@ console.log(typeof name);
 console.log(typeof isStudent);
 console.log(typeof hobbies);
 console.log(typeof info);
+
+/* 
+- Create variables a and b, and assign them integer values.
+- Use add, subtraction, multiply, divide, and modulus operator for a and b. Store each result in separate variables (sum, difference, product, quotient, reminder).
+- Calculate the remainder of a divided by b using the modulus operator and store it in a variable called remainder.
+
+- Use console.log() to print the results of each operation to the console.
+
+- Add comments in your code to explain what each operation does. */
+
+let a = 20;
+let b = 70;
+
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+let mod = a % b;
