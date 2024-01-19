1d1062db9aebb0d13901efa567eb1a6a918ccdd3
saurov-paul <saurov.paul1@gmail.com>
2024-01-19 13:09:01 +0200

    data type solution

--- Task_1/script.js
+++ Task_1/script.js
@@ -1,2 +1,27 @@
-alert("welcome to learning to JavaScript");
-console.log("Learning is fun!");
+// - Create a variable named age and assign it an integer value.
+// - Create a variable named name and assign it a string value.
+// - Create a variable named isStudent and assign it a boolean value.
+// - Create a variable named hobbies and assign it an array containing a few string values (e.g., hobbies).
+// - Create an info object with two properties: schoolName (a string) and grade (an integer).
+
+// - Use console.log() to print each variable to the console.
+
+// - Use the typeof operator to check the data type of each variable and print the result using console.log()
+
+let age = 28;
+let name = "Saurov";
+let isStudent = true;
+let hobbies = ["Game", "travelling", "cooking"];
+let info = { schooleName: "Business college", grade: 4 };
+
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info);
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof info);
