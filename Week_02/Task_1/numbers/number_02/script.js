0f566780a23321c799097cdf78cbe871dbc80f98
saurov-paul <saurov.paul1@gmail.com>
2024-01-20 22:30:50 +0200

    number_2

---
+++ Week_02/Task_1/numbers/number_02/script.js
@@ -0,0 +1,6 @@
+let fehren = 21;
+let ferhenToC = ((fehren - 32) * 5) / 6;
+
+let result = Math.round(ferhenToC);
+
+console.log(result);
--- Week_02/Task_1/numbers/numbers_01/script.js
+++ Week_02/Task_1/numbers/numbers_01/script.js
@@ -1,12 +1,36 @@
-function convertNumberToString(number) {}
+function convertNumberToString() {}
 
 function convertStringToNumber(string) {}
 
 // Sample usage - do not modify
-console.log(convertNumberToString(42)); // "42"
-console.log(convertNumberToString(97)); // "97"
-console.log(convertNumberToString(11)); // "11"
+// console.log(convertNumberToString(42)); // "42"
+// console.log(convertNumberToString(97)); // "97"
+// console.log(convertNumberToString(11)); // "11"
 
-console.log(convertStringToNumber("42")); // 42
-console.log(convertStringToNumber("97")); // 97
-console.log(convertStringToNumber("11")); // 11
+// console.log(convertStringToNumber("42")); // 42
+// console.log(convertStringToNumber("97")); // 97
+// console.log(convertStringToNumber("11")); // 11
+
+let age = 25;
+let stringAge = age.toString();
+
+console.log(stringAge);
+console.log(typeof stringAge);
+
+let name = 70;
+let numberName = Number.parseInt(name);
+
+console.log(numberName);
+console.log(typeof stringAge);
+
+let card = 555;
+let conNumber = card.toString();
+
+console.log(conNumber);
+console.log(typeof conNumber);
+
+let box = "989";
+let conString = Number.parseInt(box);
+
+console.log(conString);
+console.log(typeof conString);
