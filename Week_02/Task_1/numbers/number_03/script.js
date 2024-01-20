bd33f7e52a7389c1bafc12cb2e103713d43a3766
saurov-paul <saurov.paul1@gmail.com>
2024-01-21 00:41:26 +0200

    number_3

---
+++ Week_02/Task_1/numbers/number_03/script.js
@@ -0,0 +1,18 @@
+let num1 = 20.55;
+let num2 = 15.22;
+
+let roundnum1 = Math.round(num1);
+
+let roundnum2 = Math.round(num2);
+
+console.log(roundnum1);
+console.log(roundnum2);
+
+let sqrtNum1 = Math.sqrt(num1);
+console.log(sqrtNum1);
+console.log(typeof sqrtNum1);
+
+let maxnum = Math.max(num1, num2);
+console.log(maxnum);
+
+console.log(typeof maxnum);
